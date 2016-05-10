5d8b346d80dee68b73d60ee33ca09fad62eaf18e
jakkam girish <girish.jakkam@gmail.com>
2016-05-10 21:20:06 +0530

    post program

---
+++ post.py
@@ -0,0 +1,16 @@
+import httplib
+conn = httplib.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print r1.status, r1.reason
+
+data1 = r1.read()
+conn.request("GET", "/")
+r2 = conn.getresponse()
+
+print r2.status, r2.reason
+
+data2 = r2.read()
+
+
+conn.close()
